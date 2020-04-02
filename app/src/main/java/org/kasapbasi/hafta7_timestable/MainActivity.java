47b2bfd514e89710ccf68d42edf452e44ea67ac9
mckasap <mckasap@gmail.com>
2020-04-02 15:38:25 +0300

    Final

--- app/src/main/java/org/kasapbasi/hafta7_timestable/MainActivity.java
+++ app/src/main/java/org/kasapbasi/hafta7_timestable/MainActivity.java
@@ -3,9 +3,12 @@ package org.kasapbasi.hafta7_timestable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,10 +28,36 @@ ListView lv;
         sb.setMax(20);
         sb.setMin(1);
         sb.setProgress(10);
+    infilate(10);
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                infilate(progress);
+            }
 
-        sb.
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
 
 
+    }
+
+    private void infilate(int progress) {
+        tv.setText(""+progress);
+        ArrayList<String> mycontent= new ArrayList<>();
+        for(int i=1;i<=12;i++){
+            mycontent.add(""+progress+"*"+i+"="+(i*progress));
+        }
+        ArrayAdapter<String> adaptor = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, mycontent);
+        lv.setAdapter(adaptor);
     }
 }
